e04cf93827cab686da727ede51622056690ca695
Maik Kreutzer <s17ce7@umwelt-campus.de>
2023-12-06 13:14:16 +0100

    updated cypress performance test

--- cypress/e2e/performance.cy.ts
+++ cypress/e2e/performance.cy.ts
@@ -21,8 +21,19 @@ describe('performance spec', () => {
          */
         cy.visit('http://localhost:8080/progress');
         cy.wait(3000);
-        // select gym
+        // Select Gym "Boulder Olymp"
         cy.get('#gyms').select('Boulder Olymp');
+        cy.wait(3000);
+
+        for (let i: number = 0; i < 5; i++) {
+            cy.get('#button-plus-' + i).click();
+            cy.wait(1000);
+        }
+
+        cy.wait(3000);
+
+        // Select Gym "Rocklands"
+        cy.get('#gyms').select('Rocklands');
         cy.wait(3000);
 
         for (let i: number = 0; i < 5; i++) {
